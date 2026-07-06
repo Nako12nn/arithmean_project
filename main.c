247b21881f67509a997f1c04b1b7f722f3254441
Maxym <hako.diaha@gmail.com>
2026-07-06 10:21:59 +0200

    recursion

--- main.c
+++ main.c
@@ -1,51 +1,50 @@
 #include <stdio.h>
-#define SIZE    7
+#define N   8
 
-void filter (int new_ar[], size_t size_new,
-            const int source_arr[], size_t size_source,
-            int (*is_correct) (int))
+void show_game_field(const char (*arr_ptr)[N])
 {
-    for (int i = 0; i < size_new; ++i)
-        new_ar[i] = 0;
-    
-
-    for (int i = 0, j = 0; i < size_new; ++i) {
-        if (is_correct(source_arr[i]))
-            new_ar[j++] = source_arr[i];
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
+            printf("%c ", (arr_ptr[i][j] == 0) ? '#' : '0');
+        }
+        puts("");
     }
 }
 
-int is_even(int a) 
+void open_zeroes(const char (*private_ptr)[N], char (*public_ptr)[N], int i, int j)
 {
-    return a % 2 == 0;
+    if (private_ptr[i][j] != 0 || public_ptr[i][j] == 1)
+        return;
+    
+    public_ptr[i][j] = 1;
+
+    if ((i-1) >= 0 && private_ptr[i-1][j] == 0) open_zeroes(private_ptr, public_ptr, i-1, j);
+    if ((i+1) < N && private_ptr[i+1][j] == 0) open_zeroes(private_ptr, public_ptr, i+1, j);
+    if ((j-1) >= 0 && private_ptr[i][j-1] == 0) open_zeroes(private_ptr, public_ptr, i, j-1);
+    if ((j+1) < N && private_ptr[i][j+1] == 0) open_zeroes(private_ptr, public_ptr, i-1, j+1);
 }
 
-int is_odd(int a)
-{
-    return a % 2 != 0;
-}
+int main(void) {
 
-int is_positive(int a)
-{
-    return a > 0;
-}
+    char private_feild[N][N] = {
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 1, 1, 0, 0, 1},
+                {1, 0, 0, 1, 1, 0, 0, 1},
+                {1, 1, 0, 0, 0, 0, 1, 1},
+                {1, 1, 0, 1, 1, 0, 1, 1},
+                {1, 1, 0, 1, 1, 0, 1, 1},
+                {1, 1, 0, 0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1, 0, 0, 0},
+    };
 
-int is_negative(int a)
-{
-    return a < 0;
-}
+    char public_field[N][N] = {0};
+    show_game_field(public_field);
 
+    puts("-----------------------");
 
-int main (void) {
+    open_zeroes(private_feild, public_field, 7, 7);
 
-    int numbers[] = {2, 5, 3, 6, 5, 9, -2, 0, 5, 4};
-    int reslt[SIZE];
-    int (*criterials[]) (int) = {is_even, is_odd, is_positive, is_negative}; // array of ptrs to functions
-
-    filter(reslt, SIZE, numbers, sizeof(numbers) / sizeof(*numbers), criterials[1]);
-
-    for (int i = 0; i < SIZE; ++i) 
-        printf("%d ", reslt[i]);
+    show_game_field(public_field);
 
     return 0;
 }
