417c4556a26c163cd21a16db80335673ccfb2523
Maxym <hako.diaha@gmail.com>
2026-07-09 19:51:06 +0200

    structures

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+
+enum {name_length=50, birth_length=20};
+
+struct pers_info {
+    char name[name_length];
+    char last_name[name_length];
+};
+
+struct included_info {
+    struct pers_info nm_lm; // embedded structures 
+    char sex;
+    unsigned short age;
+    char birth_data[birth_length];
+
+};
 
 
 
 int main(void) {
 
-    char *my_arr = malloc(10); // points to this data
-    int *ptr_int_mem = malloc(sizeof(int)); // gives us 4 bytes of mamory
-    short *ptr_shtr_mem = malloc(7 * sizeof(short));
+    struct included_info exmpl_man = {
+        {"Maksym", "Nakonechnyi"},
+        'M',
+        29,
+        "20000"
+    };
 
-    free(my_arr);
-    free(ptr_int_mem);
-    free(ptr_shtr_mem);
+    printf("Name = %s\nlastname = %s\n", exmpl_man.nm_lm.name, exmpl_man.nm_lm.last_name);
+
+    exmpl_man.age = 16;
+    strcpy(exmpl_man.birth_data, "31.05.2007");
+
+    printf("new age = %d\nnew birth data = %s\n", exmpl_man.age, exmpl_man.birth_data);
 
     return 0;
 }
