15d20e342e586abcc34b5a56c66f5bbfd5dd018b
Maxym <hako.diaha@gmail.com>
2026-07-20 21:01:38 +0200

    back to c

--- func.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MYFUNC_H__
-#define __MYFUNC_H__
-
-double find_rectangle_perymetr(double, double);
-
-#endif
--- tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-strcat(str1, str2);
-    // printf("%s", str1);
-    // printf("\n");
